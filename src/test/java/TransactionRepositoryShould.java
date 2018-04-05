15228678099dabd2853585194193b04bf99cb690
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-04-05 16:00:10 +0100

    add TransactionRepository test

---
+++ src/test/java/TransactionRepositoryShould.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TransactionRepositoryShould {
+
+    @Test
+    public void
+    retrieveAll_transaction_in_proper_order() {
+        Transaction depositTransaction = new Transaction(1000, "10-01-2012");
+        Transaction withdrawalTransaction = new Transaction(-500, "14-01-2012");
+        TransactionRepository transactionRepository = new TransactionRepository();
+        transactionRepository.add(depositTransaction);
+        transactionRepository.add(withdrawalTransaction);
+
+        List<Transaction> actualTransactions = transactionRepository.retrieveAll();
+
+        assertEquals(2, actualTransactions.size());
+        assertEquals(depositTransaction, actualTransactions.get(0));
+        assertEquals(withdrawalTransaction, actualTransactions.get(1));
+
+
+    }
+}
