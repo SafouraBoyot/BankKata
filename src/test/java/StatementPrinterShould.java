02c1410db1bbdf03728906f64fdd13da74aa0792
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-04-05 17:41:02 +0100

    Refactor StatementPrinterShould with Order

--- src/test/java/StatementPrinterShould.java
+++ src/test/java/StatementPrinterShould.java
@@ -3,13 +3,14 @@ import com.codurance.StatementPrinter;
 import com.codurance.Transaction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.inOrder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StatementPrinterShould {
@@ -28,11 +29,12 @@ public class StatementPrinterShould {
         StatementPrinter statementPrinter = new StatementPrinter(consoleOutput);
 
         statementPrinter.printStatement(transactions);
+        InOrder inOrder = inOrder(consoleOutput);
 
-        verify(consoleOutput).printLine("date || credit || debit || balance");
-        verify(consoleOutput).printLine("14/01/2012 || || 500.00 || 2500.00");
-        verify(consoleOutput).printLine("13/01/2012 || 2000.00 || || 3000.00");
-        verify(consoleOutput).printLine("10/01/2012 || 1000.00 || || 1000.00");
+        inOrder.verify(consoleOutput).printLine("date || credit || debit || balance");
+        inOrder.verify(consoleOutput).printLine("14/01/2012 || || 500.00 || 2500.00");
+        inOrder.verify(consoleOutput).printLine("13/01/2012 || 2000.00 || || 3000.00");
+        inOrder.verify(consoleOutput).printLine("10/01/2012 || 1000.00 || || 1000.00");
 
 
 
